2dbf6471f84326b26cd731aa60da41fa4fe01a08
Vincent Latrouite <vincent.latrouite@epita.fr>
2012-07-10 20:25:09 +0200

    we can now view the image but not with filters, must work on it

--- src/filter/CannyFilter.cc
+++ src/filter/CannyFilter.cc
@@ -9,9 +9,12 @@ void*
 CannyFilter::operator()(void* elt)
 {
   IplImage* img = static_cast<IplImage*> (elt);
+  if (!img)
+    printf("img null in canny filter\n");
   IplImage* out = 0;
   cvCanny(img, out, 200 * 49, 600 * 49, 7);
-  return out;
+
+  return img;
 }
 
 std::string
