11c5c648f07320f357af72278dded7a234282ab1
berson_r <berson_r@epita.fr>
2012-07-10 20:03:04 +0200

    fixed arguments grabing in main

--- src/main.cc
+++ src/main.cc
@@ -14,28 +14,29 @@ using namespace cv;
 
 int test(parsepit::Driver& drv, int threads)
 {
-  tbb::task_scheduler_init init( threads );
+  tbb::task_scheduler_init init(threads);
   //The pipeline.
   tbb::pipeline pipeline;
 
   //First we init the inputs and outputs.
-  CvCapture* capture = cvCaptureFromFile(drv.input_get ()->c_str());
+  //CvCapture* capture = cvCaptureFromFile(drv.input_get ()->c_str());
+  CvCapture* capture = cvCreateCameraCapture(0);
 
   IplImage* img = 0;
-  if(!cvGrabFrame(capture))
+  if (!cvGrabFrame(capture))
   {              // capture a frame 
       printf("Could not grab a frame\n\7");
-        exit(0);
+      exit(0);
   }
-  img=cvRetrieveFrame(capture);           // retrieve the captured frame
+  img = cvRetrieveFrame(capture);           // retrieve the captured frame
   cvQueryFrame(capture); // this call is necessary to get correct capture properties
   int frameH    = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
   int frameW    = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
   int fps       = 30;//(int)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
   int isColor = 1;
 
-  CvVideoWriter* writer = cvCreateVideoWriter(drv.output_get ()->c_str(),CV_FOURCC('P','I','M','1'),
-                                 fps, cvSize(frameW,frameH),isColor);
+  CvVideoWriter* writer = cvCreateVideoWriter(drv.output_get ()->c_str(), CV_FOURCC('P','I','M','1'),
+                                 fps, cvSize(frameW, frameH), isColor);
   //We feed the pipeline with filters.
   InputFilter ifilter (capture);
   //Input
@@ -50,7 +51,6 @@ int test(parsepit::Driver& drv, int threads)
   pipeline.add_filter (ofilter);
   //Then we can run the pipeline.
   pipeline.run(threads);
-
 
   //We release the inputs and outputs and clear the pipeline.
   pipeline.clear();
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
     std::cout << nthread << std::endl;
     parsepit::Driver d;
 
-    if (argc == 2)
+    if (argc >= 2)
         d.parse_file(*new std::string(argv[1]));
     test (d, nthread);
 
