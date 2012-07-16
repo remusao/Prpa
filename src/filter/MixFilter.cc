2cabf4b4981fb429c32af076cb546b6d8e85dd52
Vincent Latrouite <vincent.latrouite@epita.fr>
2012-07-16 15:42:56 +0200

    seems to work... sometimes...

--- src/filter/MixFilter.cc
+++ src/filter/MixFilter.cc
@@ -1,7 +1,8 @@
 #include "MixFilter.hh"
+#include "string.h"
 
 MixFilter::MixFilter()
-  : filter (/* is_serial */ false)
+  : filter (/* is_serial */ true)
 {
 }
 
@@ -15,15 +16,25 @@ MixFilter::operator()(void* elt)
   if (!img || !img2)
     printf("img null in canny filter\n");
   IplImage* out;
-  out = cvCreateImage(cvGetSize(img2), IPL_DEPTH_8U, img2->nChannels);
+  out = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, img->nChannels);
 
-  char* data = out->imageData;
-  int height = out->height;
-  int width = out->width;
-  int step = out->widthStep;
-  int channels = out->nChannels;
-for (int i = 0; i < img->imageSize; i++) 
-     data[i] = 255 - img2->imageData[i];
+  int height = img->height;
+  int width = img->width;
+  int channels = img->nChannels;
+  int step = img->widthStep;
+  unsigned char* data = reinterpret_cast<unsigned char*>(out->imageData);
+
+  //std::cout << height << " " << width << " " << channels << " " << step << " " << img->imageSize << std::endl;
+  //std::cout << out->height << " " << out->width << " " << out->nChannels << " " << out->widthStep << " " << out->imageSize << std::endl;
+
+  for(int i=0;i<img->imageSize;i++)
+  {
+     data[i] = img->imageData[i];
+  }
+  for(int i=0;i<img2->imageSize;i+=3)
+  {
+     data[i] = img2->imageData[i];
+  }
   std::pair<IplImage*, IplImage*>* pair2
     = new std::pair<IplImage*, IplImage*>(out, pair->second);
   return pair2;
